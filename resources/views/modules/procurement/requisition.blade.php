14290f528c859fec6bdd07f41f51b410057c292d
Md. Arif Khan <mak0099@gmail.com>
2018-09-11 19:04:05 +0600

    add procurement view files

---
+++ resources/views/modules/procurement/requisition.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('title', 'Purchase Requisition')
+@section('content')
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Dashboard</h3>
+      </div>
+    </div>
+    <div class="clearfix"></div>
+    {{-- Content here --}}
+    
+  </div>
+</div>
+@endsection

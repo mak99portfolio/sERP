5844e915252c148d839a98305b74ce9b34c6e1bf
Md. Arif Khan <mak0099@gmail.com>
2018-09-12 10:41:30 +0600

    modify web  routes

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Auth::routes();
-//Procurement
-Route::resource('purchase-requisition', 'PurchaseRequisitionController');
-Route::resource('purchase-order', 'PurchaseOrderController');
-Route::resource('proforma-invoice', 'ProformaInvoiceController');
-Route::resource('letter-of-credit', 'LetterOfCreditController');
-Route::resource('cost-sheet', 'CostSheetController');
-Route::resource('insurance-cover-note', 'InsuranceCoverNoteController');
-Route::resource('commercial-invoice', 'CommercialInvoiceController');
-Route::resource('packing-list', 'PackingListController');
-Route::resource('bill-of-lading', 'BillOfLadingController');
-Route::resource('cnf', 'CnfController');
-
-//Inventory
-Route::resource('working-unit', 'WorkingUnitController');
-
 Route::get('/test', 'TestController@index');
-
-
-
-Route::get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
-Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
-
-
-Route::namespace('Core')->prefix('core')->group(function(){
-
-    Route::get('cities', 'CityController@index')->name('core.cities');
+Route::namespace('Dev')->prefix('dev')->group(function(){
 
 });
 
+//Common
+Auth::routes();
+Route::get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
+Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
+
+//Core
+Route::namespace('Core')->prefix('core')->group(function(){
+    Route::get('cities', 'CityController@index')->name('core.cities');
+});
+
+//Procurement
 Route::namespace('Procurement')->prefix('procurement')->group(function(){
+    Route::resource('purchase-requisition', 'PurchaseRequisitionController');
+    Route::resource('purchase-order', 'PurchaseOrderController');
+    Route::resource('proforma-invoice', 'ProformaInvoiceController');
+    Route::resource('letter-of-credit', 'LetterOfCreditController');
+    Route::resource('cost-sheet', 'CostSheetController');
+    Route::resource('insurance-cover-note', 'InsuranceCoverNoteController');
+    Route::resource('commercial-invoice', 'CommercialInvoiceController');
+    Route::resource('packing-list', 'PackingListController');
+    Route::resource('bill-of-lading', 'BillOfLadingController');
+    Route::resource('cnf', 'CnfController');
+});
 
-    Route::get('packing-list', 'PackingController@index')->name('procurement.packing_list');
-
+//Inventory
+Route::namespace('Inventory')->prefix('inventory')->group(function(){
+    Route::resource('working-unit', 'WorkingUnitController');
 });

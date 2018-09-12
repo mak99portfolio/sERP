4cb93b810b872acedbaddf474f01136edca9a683
Asraful Islam <asrful3161@gmail.com>
2018-09-12 14:08:32 +0600

    add new controller accessMatrix

---
+++ app/Http/Controllers/Dev/AccessMatrix.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Dev;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AccessMatrix extends Controller{
+
+    public function roles(){
+
+    }
+
+    public function permissions(){
+
+    }
+
+}
--- database/seeds/WorkingUnitTypeSeeder.php
+++ database/seeds/WorkingUnitTypeSeeder.php
@@ -7,9 +7,13 @@ class WorkingUnitTypeSeeder extends Seeder{
     public function run(){
         
         $data=[
+        	['name'=>'Branch'],
         	['name'=>'Central Depot'],
+        	['name'=>'Central Warehouse'],
         	['name'=>'Depot'],
-        	['name'=>'Warehouse'],
+        	['name'=>'Factory'],
+        	['name'=>'Head Office'],
+        	['name'=>'Warehouse']
         ];
 
         \DB::table('working_unit_types')->insert($data);

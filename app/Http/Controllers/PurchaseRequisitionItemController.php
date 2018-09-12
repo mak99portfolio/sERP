b77805be92433458010efdde7c32dadf937e5961
shah rakibur rahman <libon53@gmail.com>
2018-09-12 12:05:16 +0600

    database table rename and rearrange

---
+++ app/Http/Controllers/PurchaseRequisitionItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PurchaseRequisitionItem;
+use Illuminate\Http\Request;
+
+class PurchaseRequisitionItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PurchaseRequisitionItem  $purchaseRequisitionItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PurchaseRequisitionItem $purchaseRequisitionItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PurchaseRequisitionItem  $purchaseRequisitionItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PurchaseRequisitionItem $purchaseRequisitionItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PurchaseRequisitionItem  $purchaseRequisitionItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PurchaseRequisitionItem $purchaseRequisitionItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PurchaseRequisitionItem  $purchaseRequisitionItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PurchaseRequisitionItem $purchaseRequisitionItem)
+    {
+        //
+    }
+}
---
+++ database/factories/PurchaseRequisitionItemFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PurchaseRequisitionItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

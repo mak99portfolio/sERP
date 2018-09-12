4a74d0799292b5a1c2284ebc67166faed0e9abd3
Mohammad Ali <ali.shadowhite@gmail.com>
2018-09-12 13:43:46 +0600

    working unit view some change

--- resources/views/modules/inventory/working_unit.blade.php
+++ resources/views/modules/inventory/working_unit.blade.php
@@ -5,7 +5,7 @@
   <div class="">
     <div class="page-title">
       <div class="title_left">
-        <h3>Working Unit</h3>
+        <h3>Inventory</h3>
       </div>
     </div>
     <div class="clearfix"></div>
@@ -16,34 +16,60 @@
                                 <div class="x_panel">
                                     <div class="x_title">
                                         <h2>Working Unit <small>Form</small></h2>
+                                        <a href="" class="btn btn-primary btn-sm pull-right">Working List</a>
                                         <div class="clearfix"></div>
                                     </div>
                                     <div class="x_content">
                                         <br />
                                         
                                         {{-- <form class="form-horizontal form-label-left"> --}}
-                                        {{ BootForm::horizontal() }}
+                                        {{ BootForm::horizontal(['left_column_class' => 'col-md-4 col-xs-12 col-sm-6',  'right_column_class' => 'col-md-8 col-xs-12 col-sm-6']) }}
                                             
-                                            <div class="col-md-6 col-sm-6 col-xs-12">
-                                                <div class="form-group">
-                                                    <label class="control-label col-md-3 col-sm-4 col-xs-12">Unit Name</label>
-                                                    <div class="col-md-9 col-sm-8 col-xs-12">
-                                                        <input type="text" class="form-control input-sm" placeholder="Default Input">
-                                                    </div>
-                                                </div>
-                                            </div>
+                                        
 
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                            {{ BootForm::text('unit_name','Unit Name',null,['class'=>'form-control input-sm']) }}
+                                            </div>
+                                           
                                             <div class="col-md-6 col-sm-6 col-xs-12">
                                             {{ BootForm::select('working_unit_type_id', 'Select Unit Type', $working_unit_types, 2, ['class'=>'form-control input-sm']) }}
                                             </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                            {{ BootForm::select('parent_unit_type_id', 'Parent Unit Type', $working_unit_types, 2, ['class'=>'form-control input-sm']) }}
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                            {{ BootForm::select('in_charge_employee_id', 'Select In-charge', $working_unit_types, 2, ['class'=>'form-control input-sm']) }}
+                                            </div>
                                            
                                             
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                            {{ BootForm::text('geo_location','Geo Location',null,['class'=>'form-control input-sm']) }}
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                            {{ BootForm::textarea('address','Address',null,['class'=>'form-control input-sm','rows'=>'2']) }}
+                                            </div>
+                                            <div class="col-md-12">
+<br />
+	
+<div class="ln_solid"></div>
+
+ <div class="form-group">
+    
+ 
+
+ <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+    
+  </div>
+                                           
                                         </form>
                                         
                                     </div>
+                                    
                                 </div>
                             </div>
                         </div>
+                        
 
     {{-- Content end --}}
   </div>

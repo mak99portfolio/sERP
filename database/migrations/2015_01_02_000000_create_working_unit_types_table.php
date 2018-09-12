186aa17b488eb8ff94ee9f99161bfb88f322076e
Asraful Islam <asrful3161@gmail.com>
2018-09-12 10:38:10 +0600

    update some migrations

---
+++ database/migrations/2015_01_02_000000_create_working_unit_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkingUnitTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+
+        Schema::create('working_unit_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+
+        Schema::dropIfExists('working_unit_types');
+
+    }
+}

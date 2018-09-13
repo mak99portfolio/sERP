3bcc82379aede86d2299b33448ea8ebe5f241388
Asraful Islam <asrful3161@gmail.com>
2018-09-13 12:48:47 +0600

    change migration orders

--- app/Http/Controllers/Inventory/WorkingUnitController.php
+++ app/Http/Controllers/Inventory/WorkingUnitController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Inventory;
 use App\Http\Controllers\Controller;
 use App\WorkingUnit;
 use Illuminate\Http\Request;
+use App\Helpers\Paginate;
 
 class WorkingUnitController extends Controller{
 
     public function index(){
 
-    	echo 'Moved to http://localhost:8000/inventory/working-unit/create url';
+    	$data=[
+    		'paginate'=>new Paginate('\App\WorkingUnit', ['id'=>'ID','name'=>'Name']),
+    		'carbon'=>new \Carbon\Carbon
+    	];
+
+    	dd($data['paginate']);
+
+    	return view('modulles.inventory.working_units', $data);
 
     }
 
@@ -26,7 +34,7 @@ class WorkingUnitController extends Controller{
     		'users'=>\App\User::pluck('name', 'id') //Need to filter according to employee profile
     	];
 
-        return view('modules.inventory.working_unit', $data);
+        return view('modules.inventory.working_unit_form', $data);
 
     }
 
@@ -36,14 +44,20 @@ class WorkingUnitController extends Controller{
     	//dd($request->all());
 
     	$request->validate([
-    		'name'=>'required|exists:working_units',
+    		'name'=>'required|unique:working_units,name',
+    		'sort_name'=>'required|unique:working_units,sort_name',
     		'working_unit_type_id'=>'required|integer',
-    		'parent_unit_id'=>'required|integer',
+    		'parent_unit_id'=>'nullable|integer',
     		'in_charge'=>'required|integer',
+    		'company_id'=>'required|integer',
+    		'country_id'=>'required|integer',
+    		'division_id'=>'required|integer',
+    		'district_id'=>'required|integer',
     		'address'=>'required|max:500',
     	]);
 
-
+    	$workingUnit=\App\WorkingUnit::create($request->all());
+    	return back()->with('success', 'Form submitted successfully');
 
     }
 

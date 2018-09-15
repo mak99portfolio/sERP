abeca131253361150a0945b4130f0690bd8a74ce
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-09-15 16:18:29 +0600

    City UI

--- app/City.php
+++ app/City.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'country_id'
+    ];
+
+    public function country(){
+        return $this->belongsTo('App\Country');
+    }
 }

eb210290c6c8354fa027a15c6b2444163a79d97e
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-09-16 17:12:13 +0600

    cost and consignment particular db

--- app/ConsignmentParticular.php
+++ app/ConsignmentParticular.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ConsignmentParticular extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'country_id'
+    ];
 }

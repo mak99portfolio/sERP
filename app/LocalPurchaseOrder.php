b0a0e768fc5aece9500bf726974e143f873af778
Mohammad Ali <ali.shadowhite@gmail.com>
2018-09-18 15:30:57 +0600

    purchase create some change

--- app/LocalPurchaseOrder.php
+++ app/LocalPurchaseOrder.php
@@ -3,8 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class LocalPurchaseOrder extends Model
 {
-    //
+     use SoftDeletes;
+    protected $fillable = [
+        'purchase_oder_no',
+        'inco_terms',
+        'inco_term_info',
+        'procurement_type',
+        'purchase_order_type',
+        'status',
+        'remarks',
+        'purchase_oder_date'
+    ];
 }
--- resources/views/modules/procurement/local/purchase_order/create.blade.php
+++ resources/views/modules/procurement/local/purchase_order/create.blade.php
@@ -20,7 +20,9 @@
                     </div>
                     <div class="x_content">
                         <br />
-                        <form class="form-horizontal form-label-left">
+                    
+                             <form class="form-horizontal form-label-left" action="{{route('local-purchase-order.store')}}" method="POST">
+                        @csrf
                             <fieldset>
                                 <legend>Vendor Information:</legend>
                                 <div class="row">
@@ -75,7 +77,7 @@
                                         {{ BootForm::select('payment_method', 'Select Payment Method', ['Cash' => 'Cash', 'Cheque' => 'Cheque', 'LC' => 'LC'], null, ['class'=>'form-control input-sm']) }}
                                     </div>
                                     <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-                                        {{ BootForm::textarea('remark','Remarks',null,['class'=>'form-control input-sm','rows'=>'2']) }}
+                                        {{ BootForm::textarea('remarks','Remarks',null,['class'=>'form-control input-sm','rows'=>'2']) }}
                                     </div>
                                 </div>
                             </fieldset>

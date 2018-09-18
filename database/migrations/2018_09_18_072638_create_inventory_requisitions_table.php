817fcf624b617725299100871f361ba4927b7301
Asraful Islam <asrful3161@gmail.com>
2018-09-18 14:00:48 +0600

    working on inventory requisition

---
+++ database/migrations/2018_09_18_072638_create_inventory_requisitions_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoryRequisitionsTable extends Migration{
+
+    public function up(){
+
+        Schema::create('inventory_requisitions', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('inventory_requisition_id')->unique();
+            $table->date('date')->nullable();
+
+            $table->integer('inventory_requisition_type_id')->unsigned();
+            $table->integer('sender_depot_id')->unsigned();
+            $table->integer('requested_depot_id')->unsigned();
+            $table->integer('inventory_item_status_id')->unsigned();
+            $table->integer('initial_approver_id')->unsigned();
+            $table->integer('final_approver_id')->unsigned();
+            $table->integer('creator_user_id')->unsigned();
+            $table->integer('updator_user_id')->unsigned();
+            $table->text('remarks')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('inventory_requisition_type_id')->references('id')->on('inventory_requisition_types')->onDelete('cascade');
+            $table->foreign('sender_depot_id')->references('id')->on('working_unit_id')->onDelete('cascade');
+            $table->foreign('requested_depot_id')->references('id')->on('working_unit_id')->onDelete('cascade');
+            $table->foreign('inventory_item_status_id')->references('id')->on('inventory_item_statuses')->onDelete('cascade');
+            $table->foreign('initial_approver_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('final_approver_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('creator_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('updator_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+
+        Schema::dropIfExists('inventory_requisitions');
+
+    }
+}

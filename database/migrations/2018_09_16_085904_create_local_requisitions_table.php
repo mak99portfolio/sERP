d380b73b863b12619a7f87d7c56a133f8ea7810a
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-09-18 15:54:47 +0600

    local requisition migratition

--- database/migrations/2018_09_16_085904_create_local_requisitions_table.php
+++ database/migrations/2018_09_16_085904_create_local_requisitions_table.php
@@ -15,6 +15,23 @@ class CreateLocalRequisitionsTable extends Migration
     {
         Schema::create('local_requisitions', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('requisition_priority_id')->unsigned();
+            $table->foreign('requisition_priority_id')->references('id')->on('requisition_priorities')->onDelete('restrict');
+            $table->integer('purpose_id')->unsigned();
+            $table->foreign('purpose_id')->references('id')->on('requisition_purposes')->onDelete('restrict');
+            $table->string('requisition_no')->nullable();
+            $table->string('requisition_title')->nullable();
+            $table->string('issued_date');
+            $table->string('date_expected')->nullable();
+            $table->text('note')->nullable();
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('restrict');
+            $table->integer('creator_user_id')->unsigned();
+            $table->foreign('creator_user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->integer('updator_user_id')->unsigned()->nullable();
+            $table->foreign('updator_user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->tinyInteger('status')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }

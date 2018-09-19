c79cd2b3516787641f08204f648a02b143653a41
shah rakibur rahman <libon53@gmail.com>
2018-09-19 17:12:07 +0600

    add table of commercial invoice tracking table

--- app/CommercialInvoiceTracking.php
+++ app/CommercialInvoiceTracking.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class CommercialInvoiceTracking extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'commercial_invoice_id',
+        'bl_issue_date',
+        'document_arrived_at_bank_date',
+        'document_send_at_port_date',
+        'document_value_payment_date',
+        'container_arrived_at_port_date',
+        'container_birth_at_port_date',
+        'container_delivery_at_port_date',
+        'receive_at_warehouse_date',
+        'city_id',
+        'city_id',
+        
+    ];
+    public function commercial(){
+        return $this->belongsTo('App\CommercialInvoice');
+    }
 }

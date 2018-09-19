2052eab87490957a61c9b986690cd191bcead1c4
Mohammad Ali <ali.shadowhite@gmail.com>
2018-09-19 13:35:03 +0600

    letterofcreditetableseeder add

--- database/seeds/LetterOfCreditTableSeeder.php
+++ database/seeds/LetterOfCreditTableSeeder.php
@@ -33,9 +33,32 @@ class LetterOfCreditTableSeeder extends Seeder
                 'transhipment_information'=>1,
                 'company_id'=>1,
                 'creator_user_id'=>1,
+                ],
+          [
+                'letter_of_credit_no'=>112,
+                'letter_of_credit_date'=>\Carbon\Carbon::now(),
+                'letter_of_credit_value'=>10000,
+                'vendor_id'=>1,
+                'letter_of_credit_expire_date'=>\Carbon\Carbon::now(),
+                'letter_of_credit_status'=>1,
+                'letter_of_credit_shipment_date'=>\Carbon\Carbon::now(),
+                'currency'=>1,
+                'beneficiary_ac_no'=>23441,
+                'beneficiary_ac_name'=>"ribon",
+                'beneficiary_branch_name'=>"Panthopath branch",
+                'beneficiary_bank_name'=>1,
+                'issue_ac_no'=>1,
+                'issue_ac_name'=>1,
+                'issue_branch_name'=>1,
+                'issue_bank_name'=>1,
+                'partial_shipment'=>1,
+                'transhipment_information'=>1,
+                'company_id'=>1,
+                'creator_user_id'=>1,
                 ]
+          
         ];
 
-        \DB::table('foreign_requisitions')->insert($data);
+        \DB::table('letter_of_credits')->insert($data);
     }
 }

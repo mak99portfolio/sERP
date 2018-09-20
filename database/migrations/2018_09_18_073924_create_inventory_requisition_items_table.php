1a4aeef1b16559aaf5ada739688c6ec0a33939e5
Asraful Islam <asrful3161@gmail.com>
2018-09-20 10:37:46 +0600

    add item pattern in inventory

--- database/migrations/2018_09_18_073924_create_inventory_requisition_items_table.php
+++ database/migrations/2018_09_18_073924_create_inventory_requisition_items_table.php
@@ -12,12 +12,16 @@ class CreateInventoryRequisitionItemsTable extends Migration{
             $table->increments('id');
             $table->integer('inventory_requisition_id')->unsigned();
             $table->integer('product_id')->unsigned();
+            $table->integer('inventory_item_status_id')->unsigned()->nullable();
+            $table->integer('inventory_item_pattern_id')->unsigned()->nullable();
             $table->integer('requested_quantity')->unsigned()->default(0);
             $table->softDeletes();
             $table->timestamps();
 
             $table->foreign('inventory_requisition_id')->references('id')->on('inventory_requisitions')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('inventory_item_status_id')->references('id')->on('inventory_item_statuses')->onDelete('cascade');
+            $table->foreign('inventory_item_pattern_id')->references('id')->on('inventory_item_patterns')->onDelete('cascade');
         });
 
     }

2e4c8ba6c0f95a35d12d323a521756483eec5356
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-09-20 16:08:40 +0600

    cost sheet model

--- database/migrations/2018_09_11_134155_create_cost_sheets_table.php
+++ database/migrations/2018_09_11_134155_create_cost_sheets_table.php
@@ -31,44 +31,6 @@ class CreateCostSheetsTable extends Migration
             $table->softDeletes();
             $table->timestamps();
         });
-
-        Schema::create('cost_sheet_particulars', function (Blueprint $table) {
-            $table->increments('id');
-            $table->double('percent_of_lc_margin');
-            $table->double('amount_of_lc_margin');
-            $table->double('round_amount_of_lc_margin');
-            $table->double('percent_of_lc_commision');
-            $table->double('amount_of_lc_commision');
-            $table->double('round_amount_of_lc_commision');
-            $table->double('percent_of_vat');
-            $table->double('amount_of_vat');
-            $table->double('round_amount_of_vat');
-            $table->double('percent_of_swift');
-            $table->double('amount_of_swift');
-            $table->double('round_amount_of_swift');
-            $table->double('percent_of_stamp_charge');
-            $table->double('amount_of_stamp_charge');
-            $table->double('round_amount_of_stamp_charge');
-            $table->double('percent_of_stamp_charge');
-            $table->double('amount_of_stamp_charge');
-            $table->double('round_amount_of_stamp_charge');
-            $table->double('percent_of_lcaf_issue_charge');
-            $table->double('amount_of_lcaf_issue_charge');
-            $table->double('round_amount_of_lcaf_issue_charge');
-            $table->double('percent_of_imp');
-            $table->double('amount_of_imp');
-            $table->double('round_amount_of_imp');
-            $table->double('percent_of_lc_application_form');
-            $table->double('amount_of_lc_application_form');
-            $table->double('round_amount_of_lc_application_form');
-            $table->double('percent_of_others')->nullable();
-            $table->double('amount_of_others')->nullable();
-            $table->double('round_amount_of_others')->nullable();
-            $table->integer('cost_sheet_id')->unsigned();
-            $table->foreign('cost_sheet_id')->references('id')->on('cost_sheets')->onDelete('cascade');
-            $table->softDeletes();
-            $table->timestamps();
-        });
     }
 
     /**
---
+++ database/migrations/2018_09_20_095625_create_cost_sheet_particulars_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCostSheetParticularsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cost_sheet_particulars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('percent_of_lc_margin');
+            $table->double('amount_of_lc_margin');
+            $table->double('round_amount_of_lc_margin');
+            $table->double('percent_of_lc_commision');
+            $table->double('amount_of_lc_commision');
+            $table->double('round_amount_of_lc_commision');
+            $table->double('percent_of_vat');
+            $table->double('amount_of_vat');
+            $table->double('round_amount_of_vat');
+            $table->double('percent_of_swift');
+            $table->double('amount_of_swift');
+            $table->double('round_amount_of_swift');
+            $table->double('percent_of_stamp_charge');
+            $table->double('amount_of_stamp_charge');
+            $table->double('round_amount_of_stamp_charge');
+            $table->double('percent_of_lcaf_issue_charge');
+            $table->double('amount_of_lcaf_issue_charge');
+            $table->double('round_amount_of_lcaf_issue_charge');
+            $table->double('percent_of_imp');
+            $table->double('amount_of_imp');
+            $table->double('round_amount_of_imp');
+            $table->double('percent_of_lc_application_form');
+            $table->double('amount_of_lc_application_form');
+            $table->double('round_amount_of_lc_application_form');
+            $table->double('percent_of_others')->nullable();
+            $table->double('amount_of_others')->nullable();
+            $table->double('round_amount_of_others')->nullable();
+            $table->integer('cost_sheet_id')->unsigned();
+            $table->foreign('cost_sheet_id')->references('id')->on('cost_sheets')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cost_sheet_particulars');
+    }
+}

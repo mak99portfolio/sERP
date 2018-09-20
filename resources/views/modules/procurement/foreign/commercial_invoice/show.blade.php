86edd8194a46d2721f5f4a41f53d70b8d33cf074
Mohammad Ali <ali.shadowhite@gmail.com>
2018-09-20 16:36:45 +0600

    ci controller,model

--- resources/views/modules/procurement/foreign/commercial_invoice/show.blade.php
+++ resources/views/modules/procurement/foreign/commercial_invoice/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layout')
-@section('title', 'Bill of Lading')
+@section('title', 'Commercial Invoice')
 @section('content')
 <div class="right_col" role="main">
     <div class="">
@@ -9,6 +9,9 @@
             </div>
         </div>
         <div class="clearfix"></div>
+        
+        
+        {{$commercialInvoice->id}}
     </div>
 </div>
 @endsection

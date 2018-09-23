92c6170e1df24f473a3351101e11f5c0a321a91a
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-09-23 14:57:03 +0600

    make ready model and controller file for cnf

--- app/Cnf.php
+++ app/Cnf.php
@@ -3,8 +3,49 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cnf extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'letter_of_credit_id',
+        'commercial_invoice_id',
+        'vendor_id',
+        'consignee',
+        'bill_no',
+        'bill_date',
+        'bill_of_entry_no',
+        'bill_of_entry_date',
+        'arrival_date',
+        'delivery_date',
+        'job_no',
+        'cnf_value',
+        'usd_amount',
+        'exchange_rate',
+        'bdt_amount',
+        'total_day',
+        'duty_payment_date',
+        'consignment_particular_amount',
+        'previous_due_amount',
+        'cash_recieved_amount',
+        'note',
+        'consignment_particular_id'
+    ];
+
+    public function letter_of_credit(){
+        return $this->belongsTo('App\LetterOfCredit', 'letter_of_credit_id');
+    }
+
+    public function commercial_invoice(){
+        return $this->belongsTo('App\CommercialInvoice');
+    }
+
+    public function vendor(){
+        return $this->belongsTo('App\Vendor');
+    }
+
+    public function consignment_particular_cnf(){
+        return $this->belongsToMany('App\ConsignmentParticularCnf');
+    }
 }
--- app/Http/Controllers/Procurement/CnfController.php
+++ app/Http/Controllers/Procurement/CnfController.php
@@ -2,32 +2,72 @@
 
 namespace App\Http\Controllers\Procurement;
 
-use App\Http\Controllers\Controller;
 use App\Cnf;
+use App\ConsignmentParticularCnf;
+use App\ConsignmentParticular;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
+use Session;
 
 class CnfController extends Controller
 {
     private $view_root = 'modules/procurement/foreign/cnf/';
+
     public function index()
     {
         $view = view($this->view_root . 'index');
-        // $view->with('foo', 'bar');
-        // your code here
+        $view->with('cnf_list', Cnf::all());
         return $view;
     }
 
     public function create()
     {
         $view = view($this->view_root . 'create');
-        // $view->with('foo', 'bar');
-        // your code here
+        // $view->with('lc_list', LetterOfCredit::all());
+        // $view->with('vendor_list', Vendor::all());
+
         return $view;
     }
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'letter_of_credit_id' => 'required|unique:insurance_cover_notes',
+            'commercial_invoice_id' => 'required',
+            'vendor_id' => 'required',
+            'consignee' => 'required',
+            'bill_no' => 'required',
+            'bill_date' => 'required',
+            'bill_of_entry_no' => 'required',
+            'bill_of_entry_date' => 'required',
+            'arrival_date' => 'required',
+            'delivery_date' => 'required',
+            'job_no' => 'required',
+            'cnf_value' => 'required',
+            'usd_amount' => 'required',
+            'exchange_rate' => 'required',
+            'bdt_amount' => 'required',
+            'total_day' => 'required',
+            'duty_payment_date' => 'required',
+            'consignment_particular_amount' => 'required',
+            'previous_due_amount' => 'required',
+            'cash_recieved_amount' => 'required',
+            'note' => 'required',
+            'consignment_particular_id' => 'required'
+        ]);
+
+        $cnf = new Cnf;
+        $cnf->fill($request->input());
+        $cnf->creator_user_id = Auth::id();
+        $cnf->company_id = 1;
+        $cnf->cnf_no = time();
+        $cnf->save();
+        $consignment_particular_cnf = new ConsignmentParticularCnf;
+        $consignment_particular_cnf->fill($request->input());
+        $cnf->consignment_particular_cnf()->save($consignment_particular_cnf);
+        Session::put('alert-success', $cnf->cnf_no . " successfully created");
+        return redirect()->route('cost-sheet.index');
     }
 
     public function show(Cnf $cnf)

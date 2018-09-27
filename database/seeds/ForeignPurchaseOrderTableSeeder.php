a858009cc17e619c57983f052964a755f0dccc3c
shah rakibur rahman <libon53@gmail.com>
2018-09-27 13:15:40 +0600

    add po,pi,lc seeder

--- database/seeds/ForeignPurchaseOrderTableSeeder.php
+++ database/seeds/ForeignPurchaseOrderTableSeeder.php
@@ -13,100 +13,54 @@ class ForeignPurchaseOrderTableSeeder extends Seeder
     {
         $data = [
             [
-                'purchase_order_no' => '5452145',
-                'vendor_id' => '1',
-                'requisition_date' => \Carbon\Carbon::now(),
-                'purchase_order_date' => \Carbon\Carbon::now(),
-                'port_of_loading_port_id' => '1',
-                'port_of_discharge_port_id' => '1',
-                'final_destination_country_id' => '1',
-                'final_destination_city_id' => '1',
-                'origin_of_goods_country_id' => '1',
-                'payment_type' => '1',
-                'pre_carriage_by' => '1',
-                'subject' => 'test1',
-                'letter_header' => 'test1',
-                'letter_footer' => 'test1',
-                'notes' => 'test1',
-                'creator_user_id' => 1,
-                'updator_user_id' => 1,
-            ],
-            [
-                'purchase_order_no' => '444444',
-                'vendor_id' => '2',
-                'requisition_date' => \Carbon\Carbon::now(),
-                'purchase_order_date' => \Carbon\Carbon::now(),
-                'port_of_loading_port_id' => '2',
-                'port_of_discharge_port_id' => '2',
-                'final_destination_country_id' => '2',
-                'final_destination_city_id' => '1',
-                'origin_of_goods_country_id' => '1',
-                'payment_type' => '1',
-                'pre_carriage_by' => '1',
-                'subject' => 'test2',
-                'letter_header' => 'test2',
-                'letter_footer' => 'test2',
-                'notes' => 'test2',
-                'creator_user_id' => 1,
-                'updator_user_id' => 1,
+                'vendor_id'=>1,
+                'purchase_order_no'=>110010,
+                'requisition_date'=>'09/27/2018',
+                'purchase_order_date'=>'09/27/2018',
+                'port_of_loading_port_id'=>1,
+                'port_of_discharge_port_id'=>1,
+                'final_destination_country_id'=>1,
+                'final_destination_city_id'=>1,
+                'origin_of_goods_country_id'=>1,
+                'payment_type'=> 'Cash',
+                'pre_carriage_by'=> 'pre carriage by',
+                'subject'=>'subject',
+                'letter_header'=>'letter header',
+                'letter_footer'=>'letter footer',
+                'notes'=>'note'
             ],
         ];
+
         \DB::table('purchase_orders')->insert($data);
-        \DB::table('purchase_order_items')->insert([
+        $items = [
             [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 1,
-                'product_id' => 1,
+                'quantity'=>20,
+                'unit_price'=>127,
+                'purchase_order_id'=>1,
+                'product_id'=>1
             ],
             [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 1,
-                'product_id' => 2,
+                'quantity'=>20,
+                'unit_price'=>121,
+                'purchase_order_id'=>1,
+                'product_id'=>2
             ],
             [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 1,
-                'product_id' => 3,
+                'quantity'=>20,
+                'unit_price'=>120,
+                'purchase_order_id'=>1,
+                'product_id'=>3
             ],
+        ];
+
+        \DB::table('purchase_order_items')->insert($items);
+        $items = [
             [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 1,
-                'product_id' => 4,
-            ],
-            [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 2,
-                'product_id' => 1,
-            ],
-            [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 2,
-                'product_id' => 1,
-            ],
-            [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 2,
-                'product_id' => 2,
-            ],
-            [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 2,
-                'product_id' => 3,
-            ],
-            [
-                'quantity' => rand(1, 20),
-                'unit_price' => rand(100, 500),
-                'purchase_order_id' => 2,
-                'product_id' => 4,
-            ],
-        ]);
+                'purchase_order_id'=>1,
+                'foreign_requisition_id'=>1
+            ]
+        ];
+
+        \DB::table('foreign_requisition_purchase_order')->insert($items);
     }
 }

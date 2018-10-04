a062383045be703938493c17f05d8e0b7f6eb594
Md. Arif Khan <mak0099@gmail.com>
2018-10-04 12:17:35 +0600

    some change

--- app/Http/Controllers/Report/ProcurementReportController.php
+++ app/Http/Controllers/Report/ProcurementReportController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Report;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\PurchaseOrder;
 
 class ProcurementReportController extends Controller
 {
     private $view_root = 'modules/report/procurement/';
     public function foreign_purchase_order(){
         $view = view($this->view_root . 'foreign_purchase_order');
+        $view->with('purchase_order', PurchaseOrder::first());
         return $view;
     }
     public function proforma_invoice(){

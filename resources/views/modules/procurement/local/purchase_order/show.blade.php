f56ae7d7da07d5a8acbbe67f2bebc448ca61db7a
Anisul Islam Rohit <air.rohit999@gmail.com>
2018-10-08 16:52:40 +0600

    local purchase show page UI

--- resources/views/modules/procurement/local/purchase_order/show.blade.php
+++ resources/views/modules/procurement/local/purchase_order/show.blade.php
@@ -17,30 +17,190 @@
                         <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hidden-xs">
                             <button type="button" onclick="window.history.back();" class="btn btn-sm btn-default pull-left"><i class="fa fa-reply" aria-hidden="true"></i>&nbsp;Back</button>
                         </div>
-                        <div class="col-lg-8 col-md-8 col-sm-6 col-xs-4 hidden-xs"><h2 class="text-center">Purchase Order No: </h2></div>
+                        <div class="col-lg-8 col-md-8 col-sm-6 col-xs-4 hidden-xs"><h2 class="text-center">Local Order No: </h2></div>
                         <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 hidden-xs">
                             <button type="button" class="btn btn-sm btn-info pull-right print-btn" value='Print'><i class="fa fa-print" aria-hidden="true"></i> Print</button>
                         </div>
                         <div class="visible-xs col-xs-6"><button type="button" onclick="window.history.back();" class="btn btn-sm btn-default pull-left"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i>&nbsp;Back</button></div>
                         <div class="visible-xs col-xs-6"><button type="button" class="btn btn-sm btn-info pull-right print-btn" value='Print'><i class="fa fa-print" aria-hidden="true"></i> Print</button></div>
-                        <div class="visible-xs col-xs-12"><h2 class="text-center">Purchase Order No: </h2></div>
+                        <div class="visible-xs col-xs-12"><h2 class="text-center">Local Order No: </h2></div>
                     </div>
                     <div class="x_title">
                         <div class="clearfix"></div>
                     </div>
                     <div class="x_content">
                         <br />
-                        <div class="table-responsive print-area">
+                        <div class="table-responsive print-area font-12">
                             <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3">Vendor Information</th>
+                                    </tr>
+                                </thead>
                                 <tbody>
                                     <tr>
-                                        <td class="text-center">Local Purchase Order</td>
+                                        <td><strong>Vendor:</strong> 234</td>
+                                        <td><strong>Vendor Selection Criteria:</strong> 345</td>
+                                        <td><strong>Reference No:</strong> 345</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3"><strong>Additional Information:</strong> 234</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="3"><strong>Address:</strong> 234</td>
                                     </tr>
                                 </tbody>
                             </table>
-                            
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3">Genaral PO Information</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><strong>Purchase Oder Date:</strong> 234</td>
+                                        <td><strong>Inco-Terms:</strong> 345</td>
+                                        <td><strong>Inco-Term Info:</strong> 345</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Procurement Type:</strong> 234</td>
+                                        <td><strong>Purchase Order Type:</strong> 345</td>
+                                        <td><strong>Status:</strong> 345</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Shipping Method:</strong> 234</td>
+                                        <td><strong>Payment Method:</strong> 345</td>
+                                        <td><strong>Remarks:</strong> 345</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th colspan="2">Ship To Information</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>MAGNUM Enterprise Ltd.</td>
+                                            <td><strong>Other Ship to Address:</strong> 234</td>
+                                        </tr>
+                                    </tbody>
+                            </table>
+                            <table class="table table-bordered">
+                                <thead>
+                                        <tr>
+                                            <th colspan="4">PR Information</th>
+                                        </tr>
+                                        <tr>
+                                            <th>SL NO</th>
+                                            <th>Purchase Requisition No</th>
+                                            <th>Date</th>
+                                            <th>Purchase Requisition Name</th>
+                                        </tr>
+                                </thead>
+                                <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>4352525</td>
+                                            <td>2018-10-08 10:00:09</td>
+                                            <td>Test Requisition 2</td>
+                                        </tr>
+                                </tbody>
+                            </table>
+                            <table class="table table-bordered">
+                                    <thead>
+                                            <tr>
+                                                <th colspan="12">PO Product Details</th>
+                                            </tr>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Item Name</th>
+                                                <th>HS Code</th>
+                                                <th>Qty</th>
+                                                <th>MOU</th>
+                                                <th>Price</th>
+                                                <th>Sub Total</th>
+                                                <th>D.Rt</th>
+                                                <th>Total Disc</th>
+                                                <th>VAT Rt(%)</th>
+                                                <th>VAT Amt</th>
+                                                <th>Total (Net)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>01</td>
+                                                <td>tube </td>
+                                                <td>sdfsdfhgf</td>
+                                                <td>550</td>
+                                                <td>kilogram</td>
+                                                <td>25</td>
+                                                <td>41</td>
+                                                <td>01</td>
+                                                <td>01</td>
+                                                <td>01</td>
+                                                <td>01</td>
+                                                <td>01</td>
+                                            </tr>
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <td colspan="6">Total</td>
+                                                <td>25</td>
+                                                <td></td>
+                                                <td>85</td>
+                                                <td></td>
+                                                <td>85</td>
+                                                <td>25</td>
+                                            </tr>
+                                        </tfoot>
+                            </table>
+                            <table class="table table-bordered">
+                                    <thead>
+                                            <tr>
+                                                <th colspan="5">Payment Terms</th>
+                                            </tr>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Payment term</th>
+                                                <th>Date</th>
+                                                <th>Description</th>
+                                                <th>% or Fixed Amount</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>01</td>
+                                                <td>tube </td>
+                                                <td>02-20-2018</td>
+                                                <td>550</td>
+                                                <td>52</td>
+                                            </tr>
+                                        </tbody>
+                            </table>
+                            <table class="table table-bordered">
+                                    <thead>
+                                            <tr>
+                                                <th colspan="3">Terms and Condition</th>
+                                            </tr>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Terms and Condition</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>01</td>
+                                                <td>Warranty Terms</td>
+                                                <td>asdsa</td>
+                                            </tr>
+                                        </tbody>
+                            </table>
                             <!--start approved by-->
-                            <table id="print-footer" style="position: absolute; bottom: 20px; width: 100%; display: none;">
+                            <table id="print-footer" style="margin-top: 60px; width: 100%; display: none;">
                                 <tr>
                                     <td style="text-align: center; font-weight: bold;">
                                         <span style="border-top: 2px solid black;"> Prepared By</span>

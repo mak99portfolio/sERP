d83c0108559b00945dc11c08d67bc11bf5e6ca38
Asraful Islam <asrful3161@gmail.com>
2018-10-16 17:25:24 +0600

    working on sales challan

---
+++ database/migrations/2018_10_16_110306_create_sales_challan_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesChallanItemsTable extends Migration{
+
+    public function up(){
+
+        Schema::create('sales_challan_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sales_challan_id')->unsigned();
+            $table->integer('sales_order_id')->unsigned();
+            $table->integer('porduct_id')->unsigned();
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+
+            $table->foreign('sales_challan_id')->references('id')->on('sales_challans')->onDelete('cascade');
+            $table->foreign('sales_order_id')->references('id')->on('sales_orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+
+    public function down(){
+        Schema::dropIfExists('sales_challan_items');
+    }
+}
---
+++ database/seeds/MushakNumbersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MushakNumbersTableSeeder extends Seeder{
+
+    public function run(){
+
+    	$data=[
+    		[
+    			'name'=>'Mushak-1',
+    			'short_name'=>'MS1',
+    			'number'=>'3625-1235-14785-1224'
+    		],[
+    			'name'=>'Mushak-2',
+    			'short_name'=>'MS2',
+    			'number'=>'3625-1235-14785-1225'
+    		],[
+    			'name'=>'Mushak-3',
+    			'short_name'=>'MS3',
+    			'number'=>'3625-1235-14785-1226'
+    		],[
+    			'name'=>'Mushak-4',
+    			'short_name'=>'MS4',
+    			'number'=>'3625-1235-14785-1227'
+    		],
+    	];
+        
+    }
+
+}

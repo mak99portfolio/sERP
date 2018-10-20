05cfff31ab662ba8fbe5f762e545f3fba2708058
shah rakibur rahman <libon53@gmail.com>
2018-10-20 16:59:28 +0600

    change product set

--- database/seeds/ProductCategoryTableSeeder.php
+++ database/seeds/ProductCategoryTableSeeder.php
@@ -7,10 +7,8 @@ class ProductCategoryTableSeeder extends Seeder{
     public function run(){
 
         $data=[
-        	['parent_product_category_id'=>null, 'name'=>'Oil', 'short_name'=>'ol','creator_user_id'=>1],
-        	['parent_product_category_id'=>null, 'name'=>'Tyre', 'short_name'=>'tr','creator_user_id'=>1],
-        	['parent_product_category_id'=>1, 'name'=>'Omanoil', 'short_name'=>'ol','creator_user_id'=>1],
-        	['parent_product_category_id'=>2, 'name'=>'MRF Tyre', 'short_name'=>'mt','creator_user_id'=>1],
+        	['parent_product_category_id'=>null, 'name'=>'Oman Oil', 'short_name'=>'ol','creator_user_id'=>1],
+        	['parent_product_category_id'=>null, 'name'=>'MRF Tyre', 'short_name'=>'mt','creator_user_id'=>1],
         ];
 
         \DB::table('product_categories')->insert($data);
--- database/seeds/ProductSetTableSeeder.php
+++ database/seeds/ProductSetTableSeeder.php
@@ -13,7 +13,8 @@ class ProductSetTableSeeder extends Seeder
     {
         $data=[
         	['name'=>"TTF", 'short_name'=>'TTF', 'creator_user_id'=>1],
-        	['name'=>"TL", 'short_name'=>'TL', 'creator_user_id'=>1],
+        	['name'=>"TT", 'short_name'=>'TT', 'creator_user_id'=>1],
+        	['name'=>"T", 'short_name'=>'T', 'creator_user_id'=>1],
         ];
 
         \DB::table('product_sets')->insert($data);

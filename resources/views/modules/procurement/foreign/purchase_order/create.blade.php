16d884e758d1fc3a12afd8d35e24c5d7e2e14682
shah rakibur rahman <libon53@gmail.com>
2018-10-22 12:16:43 +0600

    Foreign Po item validate

--- resources/views/modules/procurement/foreign/purchase_order/create.blade.php
+++ resources/views/modules/procurement/foreign/purchase_order/create.blade.php
@@ -195,8 +195,8 @@
             if($scope.quantity[parentIndex][index] > $scope.max_quantity[parentIndex][index] ){
                 $scope.quantity[parentIndex][index] = $scope.max_quantity[parentIndex][index] ;
             }
-            if($scope.quantity[index]<1){
-                $scope.quantity[index] = 1;
+            if($scope.quantity[parentIndex][index]<1){
+                $scope.quantity[parentIndex][index] = 1;
             }
         }
         

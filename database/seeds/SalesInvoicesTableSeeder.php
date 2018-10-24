067c0aa7f3350681c8c08d4fcdd52ed2dd2098a6
Asraful Islam <asrful3161@gmail.com>
2018-10-24 12:22:31 +0600

    working on sales invoice

---
+++ database/seeds/SalesInvoicesTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SalesInvoicesTableSeeder extends Seeder{
+
+    public function run(){
+        
+        $data=[
+        	[
+				'sales_invoice_no'=>'INV00',
+				'sales_challan_id'=>1,
+				'sales_invoice_status'=>'delivered',
+				'sales_invoice_date'=>\Carbon\Carbon::now(),
+				'invoice_address_id'=>1,
+				'shipping_address_id'=>1,
+				'delivery_person_id'=>1
+        	],[
+				'sales_invoice_no'=>'INV00',
+				'sales_challan_id'=>1,
+				'sales_invoice_status'=>'delivered',
+				'sales_invoice_date'=>\Carbon\Carbon::now(),
+				'invoice_address_id'=>1,
+				'shipping_address_id'=>1,
+				'delivery_person_id'=>1
+        	],
+        ];
+
+    	\DB::table('sales_invoices')->insert($data);
+
+    	$data=[
+    		[
+				'sales_invoice_id'=>1,
+				'sales_challan_id'=>1,
+				'sales_order_id'=>1,
+				'product_id'=>1,
+				'bonus_quantity'=>10,
+				'invoice_quantity'=>30,
+				'discount_amount'=>300
+			],[
+				'sales_invoice_id'=>2,
+				'sales_challan_id'=>1,
+				'sales_order_id'=>1,
+				'product_id'=>1,
+				'bonus_quantity'=>10,
+				'invoice_quantity'=>30,
+				'discount_amount'=>300
+			],
+    	];
+
+    	\DB::table('sales_invoice_items')->insert($data);
+
+    	$data=[
+    		[
+    			'sales_invoice_id'=>1,
+    			'own_vehicle_id'=>1,
+    		],
+    	];
+
+    	\DB::table('sales_invoice_vehicles')->insert($data);
+
+    }
+
+}
